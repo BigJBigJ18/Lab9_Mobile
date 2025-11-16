51f2de80d0977f45a5ed1858f735538d8ba4d37b
BigJBigJ18 <rai240388@spengergasse.at>
2025-11-16 17:10:54 +0100

    Ini

---
+++ src/Mobile.java
@@ -0,0 +1,10 @@
+import java.time.LocalDate;
+
+public class Mobile {
+    private String owner;
+    private Tarif tarif;
+    private String phoneNumber;
+    private LocalDate dateOfPurchase;
+    private boolean locked;
+
+}
---
+++ src/Tarif.java
@@ -0,0 +1,3 @@
+public enum Tarif {
+    OFFICE,PRIVATE,GROUP
+}
