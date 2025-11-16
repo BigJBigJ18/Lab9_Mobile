e1aeca6855372cf74d9ac57a93fe67ecc32b6155
BigJBigJ18 <rai240388@spengergasse.at>
2025-11-16 19:49:04 +0100

    Update

--- src/Mobile.java
+++ src/Mobile.java
@@ -1,10 +1,77 @@
 import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.Date;
 
 public class Mobile {
     private String owner;
     private Tarif tarif;
     private String phoneNumber;
     private LocalDate dateOfPurchase;
-    private boolean locked;
+    private boolean locked=false;
+    private int runtime;
+
+    public Mobile(String owner, Tarif tarif, String phoneNumber, LocalDate dateOfPurchase, int runtime){
+        setOwner(owner);
+        setTarif(tarif);
+        setPhoneNumber(phoneNumber);
+        setDateOfPurchase(dateOfPurchase);
+        setRuntime(runtime);
+    }
+
+    public void setOwner(String owner) {
+        if(owner==null || owner.isBlank()){
+            owner="Max Mustermann";
+        }
+        this.owner = owner;
+    }
+
+    public void setTarif(Tarif tarif) {
+        if(tarif==null){
+            tarif=Tarif.PRIVATE;
+        }
+        this.tarif = tarif;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        if(phoneNumber==null || phoneNumber.length()<9){
+            phoneNumber="+43123456789";
+        }
+        this.phoneNumber = phoneNumber;
+    }
+
+    public void setDateOfPurchase(LocalDate dateOfPurchase) {
+        if(dateOfPurchase==null){
+            dateOfPurchase=LocalDate.of(2024, 1, 1);
+        }
+        this.dateOfPurchase = dateOfPurchase;
+    }
+
+    public void setRuntime(int runtime) {
+        if(runtime<0){
+            runtime=0;
+        }
+        this.runtime = runtime;
+    }
+
+    public String toString() {
+        return owner+"|"+tarif+"|"+phoneNumber+"|"+dateOfPurchase.format(DateTimeFormatter.ofPattern("dd.MMM.yy"))+"|"+locked+"|"+runtime;
+    }
+
+    public static Mobile getHandy(String handyString){
+        if(handyString==null){
+            return null;
+        }
+        String[] params=handyString.split("\\|");
+        if(params.length!=6){
+            return null;
+        }
+        try{
+            return new Mobile(params[0], Tarif.valueOf(params[1]), params[2], LocalDate.parse(params[3], DateTimeFormatter.ofPattern("dd.MMM.yy")), Integer.parseInt(params[5]));
+        }catch(Exception e){
+            e.printStackTrace();
+        }
+        return null;
+    }
+
 
 }
--- src/Test.java
+++ src/Test.java
@@ -1,5 +1,9 @@
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
 public class Test {
     public static void main(String[] args) {
-
+        LocalDate test=LocalDate.of(2024, 12, 31);
+        System.out.println(test.format(DateTimeFormatter.ofPattern("dd.MMM.yy")));
     }
 }
